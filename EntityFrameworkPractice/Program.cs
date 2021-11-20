e326238d1bd32071edc785044750a95a247b9992
cavid <hcavid386@gmail.con>
2021-11-20 20:35:18 +0400

    20-35

--- EntityFrameworkPractice/Program.cs
+++ EntityFrameworkPractice/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using EntityFrameworkPractice.DAL;
 using EntityFrameworkPractice.Models;
 using Microsoft.EntityFrameworkCore;
@@ -10,14 +12,35 @@ namespace EntityFrameworkPractice
     {
         static void Main(string[] args)
         {
-                using (AppDbContext Db = new AppDbContext())
-                {
-                    var product = new Product() { Name = "Lenovo", Price = 2540 };
-                    Db.Products.Add(product);
-                    Db.SaveChanges();
-                    Console.WriteLine("ok");
-                }
+            foreach (var item in GetAllProduct())
+            {
+                Console.WriteLine(item.Id+"  "+item.Name+" "+" "+item.Price+"\n");
+            }           
             
+        }
+
+        static List<Product> GetAllProduct()
+        {
+            
+            using (AppDbContext Db = new AppDbContext())
+            {
+                var product = Db.Products.ToList();
+                return product;
+
+            }
+
+
+
+        } 
+        static void AddProduct()
+        {
+            using (AppDbContext Db = new AppDbContext())
+            {
+                var product = new Product() {Name = "Lenovo", Price = 2540};
+                Db.Products.Add(product);
+                Db.SaveChanges();
+                Console.WriteLine("ok");
+            }
         }
     }
 }
